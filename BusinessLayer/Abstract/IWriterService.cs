3c251c2b4c76c6d1c656ac502b76d0d43b4f1e38
kaganefe <kagan.efe@dijitalsahne.com>
2022-10-19 17:12:51 +0300

    19.10.2022

---
+++ BusinessLayer/Abstract/IWriterService.cs
@@ -0,0 +1,16 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IWriterService
+    {
+        List<Writer> GetList();
+        void WriterAdd(Writer writer);
+        void WriterDelete(Writer writer);
+        void WriterUpdate(Writer writer);
+        Writer GetById(int id);
+    }
+}
---
+++ MvcProjeKampi/Controllers/WriterController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        public IActionResult Index()
+        {
+            var WriterValues = wm.GetList();
+            return View(WriterValues);
+        }
+    }
+}
